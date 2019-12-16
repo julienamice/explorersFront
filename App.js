8ff356c76a408cdcdfd0c70e41edc5dd37d27060
Julien Amice <amice.julien@gmail.com>
2019-12-16 13:49:08 +0100

    Base enigme Louvre git statsu

--- App.js
+++ App.js
@@ -1,25 +1,3 @@
-// import React from "react";
-// import { StyleSheet, Text, View } from "react-native";
-// import ThemeList from "./components/ThemeList";
-// import Navigation from "./components/Navigation";
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       {/* <ThemeList /> */}
-//       <Navigation />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center"
-//   }
-// });
 
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
